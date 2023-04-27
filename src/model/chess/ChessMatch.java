c90096a0b5034d132d2a0e7610dc805ec14562a5
JuanHoly <juanholydesouza@gmail.com>
2023-04-26 23:22:01 -0400

    Queen

--- src/model/chess/ChessMatch.java
+++ src/model/chess/ChessMatch.java
@@ -13,6 +13,7 @@ import model.chess.pieces.Bishop;
 import model.chess.pieces.King;
 import model.chess.pieces.Knight;
 import model.chess.pieces.Pawn;
+import model.chess.pieces.Queen;
 import model.chess.pieces.Rook;
 import model.exception.ChessException;
 
@@ -203,6 +204,7 @@ public class ChessMatch {
         
         placeNewPiece('a', 1, new Rook(board, Color.WHITE));
         placeNewPiece('b', 1, new Knight(board, Color.WHITE));
+        placeNewPiece('d', 1, new Queen(board, Color.WHITE));
         placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('e', 1, new King(board, Color.WHITE));
         placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
@@ -220,6 +222,7 @@ public class ChessMatch {
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
         placeNewPiece('b', 8, new Knight(board, Color.BLACK));
         placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
+        placeNewPiece('d', 8, new Queen(board, Color.BLACK));
         placeNewPiece('e', 8, new King(board, Color.BLACK));
         placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('g', 8, new Knight(board, Color.BLACK));
